12a6ff9e573e4ae2192085b843c9f464dd99c1b4
Anna Foix <afoix@ebi.ac.uk>
2024-08-17 11:01:37 +0100

    Fix enviroment creation script

--- setup.sh
+++ setup.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env -S bash -i
 echo "Creating conda environment"
-mamba create -n 08_knowledge_extraction python=3.11 pytorch torchvision pytorch-cuda-12.1 -c conda-forge -c pytorch -c nvidia
+mamba create -n 08_knowledge_extraction python=3.11 pytorch torchvision pytorch-cuda=12.1 -c pytorch -c nvidia
 mamba activate 08_knowledge_extraction
 pip install -r requirements.txt
