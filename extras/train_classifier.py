83495eca4c9b1bc4eb44c611c8fdb402054074d8
Diane Adjavon <adjavond@hhmi.org>
2024-08-20 14:23:19 -0400

    Fix exercise setup
    * uses conda
    * trains the classifier

--- extras/train_classifier.py
+++ extras/train_classifier.py
@@ -7,11 +7,17 @@ from classifier.data import ColoredMNIST
 import torch
 from torch.utils.data import DataLoader
 from tqdm import tqdm
+from pathlib import Path
 
 
-def train_classifier(checkpoint_dir, epochs=10):
+def train_classifier(base_dir, epochs=10):
+    checkpoint_dir = Path(base_dir) / "checkpoints"
+    checkpoint_dir.mkdir(exist_ok=True)
+    data_dir = Path(base_dir) / "data"
+    data_dir.mkdir(exist_ok=True)
+    #
     model = DenseModel((28, 28, 3), 4)
-    data = ColoredMNIST("../data", download=False, train=True)
+    data = ColoredMNIST(data_dir, download=True, train=True)
     dataloader = DataLoader(data, batch_size=32, shuffle=True, pin_memory=True)
 
     optimizer = torch.optim.Adam(model.parameters(), lr=1e-4)
@@ -30,11 +36,12 @@ def train_classifier(checkpoint_dir, epochs=10):
         print(f"Epoch {epoch}: Loss = {loss.item()}")
         losses.append(loss.item())
         # TODO save every epoch instead of overwriting?
-        torch.save(model.state_dict(), f"{checkpoint_dir}/model.pth")
+        torch.save(model.state_dict(), checkpoint_dir / "model.pth")
 
-    with open(f"{checkpoint_dir}/losses.txt", "w") as f:
+    with open(checkpoint_dir / "losses.txt", "w") as f:
         f.write("\n".join(str(l) for l in losses))
 
 
 if __name__ == "__main__":
-    train_classifier(checkpoint_dir="checkpoints", epochs=10)
+    this_dir = Path(__file__).parent
+    train_classifier(base_dir=this_dir, epochs=10)
--- setup.sh
+++ setup.sh
@@ -1,5 +1,15 @@
 #!/usr/bin/env -S bash -i
 echo "Creating conda environment"
-mamba create -n 08_knowledge_extraction python=3.11 pytorch torchvision pytorch-cuda=12.1 -c pytorch -c nvidia
-mamba activate 08_knowledge_extraction
+conda create -n 08_knowledge_extraction -y python=3.11 
+eval "$(conda shell.bash hook)"
+conda activate 08_knowledge_extraction
+# Check if the environment is activated
+echo "Environment activated: $(which python)"
+
+conda install -y pytorch torchvision pytorch-cuda=12.1 -c pytorch -c nvidia
 pip install -r requirements.txt
+
+echo "Training classifier model"
+python extras/train_classifier.py
+
+conda deactivate
